f48d52b2bdaa25eaf4686566e644701f51524f37
Rushi <rushibadheka56236@gmail.com>
2024-03-31 17:17:05 +0530

    intial code push

--- Utils/index.js
+++ Utils/index.js
@@ -2,7 +2,7 @@ import {Platform} from 'react-native';
 import {io} from 'socket.io-client';
 export const BaseUrl =
   Platform.OS === 'android'
-    ? 'http://yourIPAddress:3000/' // replace you ip address
+    ? 'http://192.168.1.26:3000/' // replace your ip
     : 'http://localhost:3000';
 
-export const socket = io.connect('http://yourIPAddress:3000');
+export const socket = io.connect('http://192.168.1.26:3000');
